a00a7fb2878bbc4b758cd854b24c492b5b73883d
Adam Keesecker <adamkeesecker@gmail.com>
2014-09-25 11:21:41 -0500

    changed css and some text. Working on live site

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923150445) do
+ActiveRecord::Schema.define(version: 20140925145035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20140923150445) do
     t.string   "uid"
     t.string   "token"
     t.string   "secret"
+    t.string   "goals"
+    t.integer  "goal_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
